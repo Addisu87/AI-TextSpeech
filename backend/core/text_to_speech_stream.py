51fc56443506fb78456f1c29671d995ac95a09d2
Addisu <addisuhaile87@gmail.com>
2025-01-15 18:59:36 +0300

    Create new route

--- backend/core/text_to_speech_stream.py
+++ backend/core/text_to_speech_stream.py
@@ -1,26 +1,28 @@
 import os
+import uuid
 
-# import uuid
+from dotenv import load_dotenv
 from elevenlabs import VoiceSettings, play
 from elevenlabs.client import ElevenLabs
 
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+load_dotenv()
 
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 if not ELEVENLABS_API_KEY:
     raise ValueError("ELEVENLABS_API_KEY environment variable not set")
+
 
 client = ElevenLabs(
     api_key=ELEVENLABS_API_KEY,
 )
 
 
-def text_to_speech_stream(text: str):
+def text_to_speech_stream(text: str) -> str:
     # Calling the text_to_speech conversion API with detailed parameters
     response = client.text_to_speech.convert(
         voice_id="pNInz6obpgDQGcFmaJgB",  # Adam pre-made voice
         model_id="eleven_multilingual_v2",
-        # text=text,
-        text="Is a new test of elevenlabs text to speech API",
+        text=text,
         output_format="mp3_22050_32",
         voice_settings=VoiceSettings(
             stability=0.0,
@@ -32,16 +34,14 @@ def text_to_speech_stream(text: str):
 
     play(response)
 
-    # # Generating a unique file name for the output MP3 file
-    # save_file_path = f"{uuid.uuid4()}.mp3"
+    # Generating a unique file name for the output MP3 file
+    save_file_path = f"{uuid.uuid4()}.mp3"
 
-    # #  Writing the audio to a file
-    # with open(save_file_path, "wb") as f:
-    #     for chunk in response:
-    #         if chunk:
-    #             f.write(chunk)
+    #  Writing the audio to a file
+    with open(save_file_path, "wb") as f:
+        for chunk in response:
+            if chunk:
+                f.write(chunk)
 
-    # print(f"A new audio file was saved successfully at {save_file_path}")
-
-    # # Return the path of the saved audio file
-    # return save_file_path
+    # Return the path of the saved audio file
+    return save_file_path
---
+++ backend/routers/text-speech.py
@@ -0,0 +1,15 @@
+from core.text_to_speech_stream import text_to_speech_stream
+from fastapi import APIRouter, HTTPException, status
+
+router = APIRouter()
+
+
+@router.post("/generate-audio", status_code=201)
+async def generate_audio_endpoint(text: str):
+    try:
+        audio = text_to_speech_stream(text)
+        return {"audio": audio}
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT, detail="Failed to generate audio"
+        )
