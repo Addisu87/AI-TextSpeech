538502ced44fca05a61a463571b5bc0dd4c736bb
Addisu <addisuhaile87@gmail.com>
2025-01-17 15:15:54 +0300

    Update folder structure

--- backend/models/character.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class CharacterRequest(BaseModel):
-    character_name: str
-    sample_audio: str  # Path to the audio file or URL
--- backend/models/text.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class TextToSpeechRequest(BaseModel):
-    character_name: str
-    text: str
